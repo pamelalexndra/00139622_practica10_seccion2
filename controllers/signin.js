ce995815a62de18e9238eec7fc3321af94c9a526
Pamelalexandragomeztenorio <00139622@uca.edu.sv>
2025-11-08 18:12:01 -0600

    feat: Agregar signin

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,16 +1,36 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 import { db } from "../data/connection.js";
+import { JWT_SECRET } from "../keys/keys.js";
 
-export const SingIn = async ( req, res ) => {
+export const SignIn = async (req, res) => {
   const { email, password } = req.body;
-  db.query('SELECT * FROM users WHERE email = $1', [email], async (error, results) => {
-    if (error) {
-      throw error 
+  db.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email],
+    async (error, results) => {
+      if (error) {
+        throw error;
+      }
+
+      const resultFind = results.rows;
+      if (resultFind.length < 1)
+        return res.status(400).json({ message: "Invalid user find" });
+
+      const userFind = resultFind[0]; // Este const hace referencia al usuario que se encontró con ese email
+
+      const isPasswordValid = await bcrypt.compare(password, userFind.password); // false si no es cierto | true si las constraseñas coinciden
+      if (!isPasswordValid)
+        return res.status(401).json({ message: "Invalid credentials" });
+
+      const _jwt = jwt.sign({ id: userFind.id }, JWT_SECRET, {
+        expiresIn: "8h",
+      });
+
+      return res
+        .status(200)
+        .json({ success: true, message: "user finded", _jwt, userFind });
     }
-
-    const resultFind = results.rows;
-    if(resultFind.length < 1)
-      return res.status(400).json({ message: "Invalid user find" });
-
-    res.status(200).json(resultFind[0]);
-  })
-}
+  );
+};
